f3e80fc1b6321d4f16882fcb38c230b35fbd948c
mohammed shoaib <shoaibme76@gmail.com>
2024-01-06 17:50:25 +0530

    new

--- script.js
+++ script.js
@@ -34,14 +34,14 @@ $(document).ready(function(){
     });
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Mechanical Engineer", "Software Developer"],
+        strings: ["Software Engineer", "Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Mechanical Engineer", "Software Developer"],
+        strings: ["Software Engineer", "Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
